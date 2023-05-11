c1cee9fddad1e023f183114b25eae90d838e2d07
Sinan Goktas <sinan.goktas@gmail.com>
2023-05-12 00:43:13 +0100

    todos added

--- automation_code/test/wp_integration/test_integration.py
+++ automation_code/test/wp_integration/test_integration.py
@@ -1,5 +1,7 @@
+# TODO Implement integration_scenarios (file in test folder)
 # TODO Implement Pytest Reporting and then extract the report from docker container
-# NOTE We will only do dockerized run for integration tests
+# NOTE We will only do dockerized run for integration tests because they will not do db validations
+# db validation can not be done against Local's db from inside a container somehow
 
 def test_integration():
     pass
